a9967fc319527e49ab758a87c2cd9ddaa1f8be3c
karim353 <karimkhaziev@mail.ru>
2024-11-22 17:45:20 +0300

    homework1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+        string digits = new string(input.Where(char.IsDigit).ToArray());
+        Console.WriteLine($"Цифры в строке: {digits}");
+    }
+}
